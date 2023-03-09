fd0c83fbd837c216f889344c43dd6906b45985bc
Daniel Weiner <danielbweiner@gmail.com>
2023-03-09 15:14:06 -0500

    More pointer hell refactoring

--- source/ui/panel.h
+++ source/ui/panel.h
@@ -18,23 +18,23 @@ public:
     void getInnerBounds(glm::vec2* topLeft, glm::vec2* bottomRight);
     void getOuterBounds(glm::vec2* topLeft, glm::vec2* bottomRight);
 private:
-    SpriteRenderer&               _spriteRenderer;
+    SpriteRenderer& _spriteRenderer;
 
-    Window&                       _window;
-    glm::vec2                     _topLeft;
-    glm::vec2                     _bottomRight;
-    glm::vec2                     _innerTopLeft;
-    glm::vec2                     _innerBottomRight;
-
-    std::unique_ptr<Sprite> const _topLeftSprite;    
-    std::unique_ptr<Sprite> const _topRightSprite;   
-    std::unique_ptr<Sprite> const _bottomLeftSprite; 
-    std::unique_ptr<Sprite> const _bottomRightSprite;
-    std::unique_ptr<Sprite> const _leftSprite;       
-    std::unique_ptr<Sprite> const _rightSprite;      
-    std::unique_ptr<Sprite> const _topSprite;        
-    std::unique_ptr<Sprite> const _bottomSprite;     
-    std::unique_ptr<Sprite> const _centerSprite;
+    Window&         _window;
+    glm::vec2       _topLeft;
+    glm::vec2       _bottomRight;
+    glm::vec2       _innerTopLeft;
+    glm::vec2       _innerBottomRight;
+   
+    Sprite&         _topLeftSprite;    
+    Sprite&         _topRightSprite;   
+    Sprite&         _bottomLeftSprite; 
+    Sprite&         _bottomRightSprite;
+    Sprite&         _leftSprite;       
+    Sprite&         _rightSprite;      
+    Sprite&         _topSprite;        
+    Sprite&         _bottomSprite;     
+    Sprite&         _centerSprite;
 
     glm::vec2 _getBorderSize();    
 };
